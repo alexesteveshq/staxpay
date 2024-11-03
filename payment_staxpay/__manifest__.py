d6b35f6d839c67cbf036dec87519b730a52555e1
alex <alexesteveshq@gmail.com>
2024-11-03 15:06:59 +0100

    [Alex] add website

--- payment_staxpay/__manifest__.py
+++ payment_staxpay/__manifest__.py
@@ -8,6 +8,7 @@
     'version': '18.0.1.0',
     'category': 'Accounting/Website',
     'author': 'Visionee',
+    'website': 'https://visionee.net',
     'license': 'OPL-1',
     'depends': ['payment'],
     'assets': {
